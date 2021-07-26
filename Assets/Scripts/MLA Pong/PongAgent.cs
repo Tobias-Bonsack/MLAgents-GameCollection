9a7fe534381d186c53cc0665f17d32b4fa3fbd12
Tobias-Bonsack <tobias.bonsack@gmail.com>
2021-07-26 19:12:10 +0200

    Pong Environment
    First easy Objects, nothing special

---
+++ Assets/Scripts/MLA Pong/PongAgent.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+public class PongAgent : Agent
+{
+
+}
