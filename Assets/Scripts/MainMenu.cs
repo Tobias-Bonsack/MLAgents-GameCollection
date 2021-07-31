3291857b579edace2dbd84e35db2dc05359e06bd
Tobias-Bonsack <tobias.bonsack@gmail.com>
2021-07-31 15:52:46 +0200

    menu for pong game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -14,4 +14,9 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void ToogleVisible(GameObject gameObject)
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
 }
