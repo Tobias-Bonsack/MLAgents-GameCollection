b4f1f4dbe3635f38a72011ef46573aa51c65738a
Tobias-Bonsack <tobias.bonsack@gmail.com>
2021-08-21 13:25:28 +0200

    import new SceneLoader :D
    it works!!!

--- Assets/Private Packages/Scene Loader/Scripts/AnimationEventObserver.cs
+++ Assets/Private Packages/Scene Loader/Scripts/AnimationEventObserver.cs
@@ -3,16 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace SceneLoader
 {
     public class AnimationEventObserver : MonoBehaviour
     {
         [SerializeField] SceneLoader.EventSystem _eventS;
+        [SerializeField] Image _loadBar;
+        [SerializeField] float _WaitAfterLoaded = 1f;
         public int _sceneNumber;
         public void EndObserver()
         {
-            SceneManager.LoadScene(_sceneNumber);
+            StartCoroutine(LoadAsyncScene());
+        }
+
+        private IEnumerator LoadAsyncScene()
+        {
+            AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(_sceneNumber);
+            asyncOperation.allowSceneActivation = false;
+
+            while (asyncOperation.progress < 0.9f)
+            {
+                Debug.Log(asyncOperation.progress);
+                yield return new WaitForEndOfFrame();
+            }
+            _loadBar.fillAmount = asyncOperation.progress;
+
+            yield return new WaitForSecondsRealtime(_WaitAfterLoaded);
+            asyncOperation.allowSceneActivation = true;
+
         }
 
         public void StartObserver()
